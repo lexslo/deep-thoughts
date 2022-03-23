ff67028763ca7209cd1a8e911c2c8721a04a8b91
Sparkle Party <lexi.slovik@gmail.com>
2022-03-23 16:05:56 -0700

    resolvers and typedefs graphql queries

--- server/server.js
+++ server/server.js
@@ -1,8 +1,31 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+const startServer = async () => {
+  // create new Apollo server and pass in our schema data
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers
+  });
+
+  // Start the Apollo server
+  await server.start();
+
+  // integrate our Apollo server with the Express app as middleware
+  server.applyMiddleware({ app });
+
+  // log URL to test GraphQL API
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+// Initialize Apollo server
+startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
